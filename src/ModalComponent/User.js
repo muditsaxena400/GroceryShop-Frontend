4c0dded27301b9ffd4b6aaf18587bc36f451258e
Manraj <manraj.singh227@gmail.com>
2021-05-18 10:48:49 +0530

    Check user model class

--- src/ModalComponent/User.js
+++ src/ModalComponent/User.js
@@ -1,22 +1,22 @@
-class User
-{
-    var id;
-    var firstName;
-    var lastName;
-    var loginId;
-    var mobileNo;
-    var email;
-    var question;
-    var answer;
-    AppUser appUser;
-    constructor(id, firstName, lastName, loginId, mobileNo, email, question, answer, appUser)
-    {
+class User {
+    constructor(id, firstName, lastName, loginId, mobileNo, email, question, answer, appUser){
         this.id =id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.loginId = loginId;
         this.mobileNo = mobileNo;
+        this.email = email;
+        this.question = question;
+        this.answer = answer;
+        this.appUser = appUser
+    }
+    
+    getId(){
+        return this.id;
+    }
 
+    setId(id){
+        this.id = id;
     }
 
 }
