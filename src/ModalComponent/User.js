df7cd62f922bb5ba38a94f1b02f0ec6dab1f27ff
arvindshokhanda <arvindshokhanda21@gmail.com>
2021-05-18 10:15:15 +0530

    first commit

---
+++ src/ModalComponent/User.js
@@ -0,0 +1,17 @@
+class User
+{
+    var id;
+    var firstName;
+    var lastName;
+    var loginId;
+    var mobileNo;
+    var email;
+    var question;
+    var answer;
+    AppUser appUser;
+    constructor(id, firstName, lastName, loginId, mobileNo, email, question, answer, appUser)
+    {
+
+    }
+
+}
